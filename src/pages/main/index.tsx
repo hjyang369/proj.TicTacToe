7397397e234f9444217205f2f0d2e2934134e6a0
hjyang369 <ghlwls6@gmail.com>
2024-02-15 16:03:36 +0900

    first commit

---
+++ src/pages/main/index.tsx
@@ -0,0 +1,8 @@
+// import S from "./style";
+export default function Main() {
+  return (
+    <div>
+      <div>메인입니다.</div>
+    </div>
+  );
+}
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+  * {
+    box-sizing: border-box;
+    font-family: 'Noto Sans KR', sans-serif;
+    color: #333;
+  }
+  button {
+    cursor: pointer;
+    background: none;
+    border: none;
+  }
+`;
+
+export default GlobalStyle;
